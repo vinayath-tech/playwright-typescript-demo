cba3a25305ae280980d4fe5f25a9e7d5e05df8b7
gokul <gokul.sridharan@blackcatsolutions.co.uk>
2024-07-05 00:36:39 +0100

    Create centralised ui actions class

---
+++ ui-test/common/web.actions.ts
@@ -0,0 +1,44 @@
+import { expect, Page } from "@playwright/test";
+
+export class WebAction {
+
+    readonly page: Page;
+
+    constructor(page: Page) {
+        this.page = page;
+    }
+
+    async navigateToUrl(expUrl: string) {
+        await this.page.goto(expUrl);
+    }
+
+    async verifyText(eleLocator: string, expText: string) {
+        await expect(this.page.locator(eleLocator))
+            .toHaveText(expText);
+    }
+
+    async verifyEleCountByLocator(eleLocator: string, expCount: number) {
+        await expect(this.page.locator(eleLocator)).toHaveCount(expCount);
+    }
+
+    async verifyEleCountByRole(eleName: string, expCount: number) {
+        await expect(this.page.getByRole('button', {name: eleName})).toHaveCount(expCount);
+    }
+
+    async enterText(eleLocator: string, inputText: string) {
+        await this.page.locator(eleLocator).fill(inputText);
+    }
+
+    async enterTextByPlaceHolder(eleName: string, inputText: string) {
+        await this.page.getByPlaceholder(eleName).fill(inputText);
+    }
+
+    async clickElement(eleLocator: string) {
+        await this.page.locator(eleLocator).click();
+    }
+
+    async clickElementByRole(eleName: string) {
+        await this.page.getByRole('button', { name: eleName}).click();
+    }
+
+}
